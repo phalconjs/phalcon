8fdef6b695efc0268e3393208aa522381afd073d
philipsz-davido <philipsz-davido>
2018-01-21 23:39:50 +0100

    Did some stuff

--- test/test.js
+++ test/test.js
@@ -2,6 +2,14 @@ var phalcon = require('../lib/phalcon');
 
 var app = new phalcon();
 
+/* set middlewares*/
+app.use((req, res) => {})
+app.use('cat', (req, res) => {})
+app.use(['dog', 'beetle'], (req, res) => {}, (req, res) => {})
+app.use(['lion'], [(req, res) => {}, (req, res) => {}])
+app.use([(req, res) => {}])
+app.use((req, res) => {}, (req, res) => {})
+
 app.get('/', (req, res) => {
     res.send('Hello World form PhalconJS');
 });
