d32ac13f1c1596250a543eea77d2da7ad9e4a7ff
philipsz-davido <philipsz-davido>
2018-01-23 15:36:29 +0100

    Did some stuff

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
+var chalk = require('chalk')
 var phalcon = require('../lib/phalcon');
 
 var app = new phalcon();
+var router = phalcon.Router()
 
 /* set middlewares*/
 app.use((req, res) => {})
@@ -13,4 +15,23 @@ app.use((req, res) => {}, (req, res) => {})
 app.get('/', (req, res) => {
     res.send('Hello World form PhalconJS');
 });
+app.listen(3000, () => {
+    console.log(chalk.green(`App started on port:`) + chalk.yellow(3000))
+});
+
+/*
+var phalcon = require('../lib/phalcon');
+
+var app = new phalcon();
+
+//Creates a new router object
+const router = phalcon.Router()
+
+router.get('get', ()=>{})
+router.route('dogs')
+    .get(()=>{});
+router.use('cat)    
+
+app.use(router)
 app.listen(3000);
+*/
